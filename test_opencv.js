cc6bbaa73e8ddb0ef646fb918db1cb5dd3c8bfa8
tung-hpzbook <tung-hpzbook@staas.io>
2026-01-18 23:09:59 +0700

    opencv.js

---
+++ test_opencv.js
@@ -0,0 +1,24 @@
+const { Jimp } = require('jimp');
+
+async function main(){
+  var jimpSrc = await Jimp.read('example/fisheye/input/IMG-0.jpg');
+
+  var src = cv.matFromImageData(jimpSrc.bitmap);
+
+  let dst = new cv.Mat();
+  let M = cv.Mat.ones(5, 5, cv.CV_8U);
+  let anchor = new cv.Point(-1, -1);
+  cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+
+  new Jimp({
+    width: dst.cols,
+    height: dst.rows,
+    data: Buffer.from(dst.data)
+  })
+  .write('output.png');
+
+  src.delete();
+  dst.delete();
+}
+
+Module = {onRuntimeInitialized: main}; cv = require('./opencv.js');
